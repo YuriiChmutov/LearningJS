39b79229fc6caf6ccce3229bb65756a7b0559f28
Yurii <vad4807@gmail.com>
2020-05-04 16:49:56 +0300

    First commit

---
+++ FirstSteps/script.js
@@ -0,0 +1,23 @@
+//"use strict";
+
+console.log("hello, world");
+
+let person = {
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+
+let arr = ['plum.png'];
+
+console.log(arr[0]);
+
+
+console.log(typeof(+'4'));
+
+let answer = prompt("are you here?");
+console.log(answer);
+
