20f7bd108314a826d4ea0e52e594bfe5b678c350
Yurii <vad4807@gmail.com>
2020-05-08 12:47:30 +0300

    add react library

--- FirstSteps/script.js
+++ FirstSteps/script.js
@@ -1,22 +1,8 @@
 //"use strict"
 
-console.log("hello, world");
+let arr = [
+    'Yura',
+     "Dima"
+];
 
-let person = {
-    name: "Jhon",
-    age: 25,
-    isMarried: false
-};
-
-console.log(person.name);
-
-
-let arr = ['plum.png'];
-
-console.log(arr[0]);
-
-
-console.log(typeof(+'4'));
-
-
-
+console.log(arr[1]);
