6256926db52acb4d1b6d6c316947019c15a91be2
Yurii <vad4807@gmail.com>
2020-07-16 22:56:00 +0300

    add first react-app

---
+++ React/react-apps/first-app/src/components/app-header/app-heder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppHeader = () => {
+    return(
+        <div className = "printInTheMiddle">
+            <h1>Yurii Chmutov</h1>
+            <h3>aka</h3>
+            <h2>Бог верстки</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ React/react-apps/first-app/src/components/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+
+const App = () => {
+    return (
+        <AppHeader/>
+    )
+}
+
+export default App;
